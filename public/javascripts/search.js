6e1328ad881ca4222cf79780504cd370b555e64a
AmeerAdhami <aadham01@dcs.bbk.ac.uk>
2020-11-07 22:35:39 +0200

    search.js refactor and clear on search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -12,47 +12,50 @@ searchForm.addEventListener('submit', async (e) => {
         type: { value: type }
     } = searchForm.elements;
     searchForm.elements.q.value = '';
+    tweetList.innerHTML = '';
+    const options = { q, model, retweets, replies, type }
 
     var max_id;
-    const options = { q, model, retweets, replies, type }
     for (let i = 0; i < count / 100; i++) {
         if (max_id) options.max_id = max_id;
         const { data } = await axios.post('/search', options);
         const { tweets } = data;
         max_id = data.max_id;
-
-        const body = document.querySelector('main');
-        for (let tweet of tweets) {
-            const li = document.createElement('li');
-            li.classList.add('media');
-            li.classList.add('mb-2');
-            li.classList.add('border');
-            li.classList.add('rounded');
-            li.classList.add('shadow-sm');
-            li.classList.add('pr-3');
-            li.classList.add(tweet.prediction == 1 ? 'pos' : 'neg');
-            const img = document.createElement('img');
-            img.src = tweet.img;
-            img.classList.add('mr-3');
-            img.classList.add('rounded-circle');
-            const div = document.createElement('div');
-            div.classList.add('media-body');
-            const h5 = document.createElement('h5');
-            h5.classList.add('mt-0');
-            h5.classList.add('mb-1');
-            h5.innerText = tweet.username;
-            const p = document.createElement('p');
-            p.classList.add('mt-0');
-            p.classList.add('mb-0');
-            p.classList.add('text-justify');
-            p.innerText = tweet.text;
-            const small = document.createElement('small');
-            small.classList.add('mt-0');
-            small.innerText = tweet.time;
-            div.append(h5, p, small);
-            li.append(img, div);
-            tweetList.append(li);
-        }
+        addTweets(tweetList, tweets);
     }
     delete max_id;
 })
+
+const addTweets = (tweetList, tweets) => {
+    for (let tweet of tweets) {
+        const li = document.createElement('li');
+        li.classList.add('media');
+        li.classList.add('mb-2');
+        li.classList.add('border');
+        li.classList.add('rounded');
+        li.classList.add('shadow-sm');
+        li.classList.add('pr-3');
+        li.classList.add(tweet.prediction == 1 ? 'pos' : 'neg');
+        const img = document.createElement('img');
+        img.src = tweet.img;
+        img.classList.add('mr-3');
+        img.classList.add('rounded-circle');
+        const div = document.createElement('div');
+        div.classList.add('media-body');
+        const h5 = document.createElement('h5');
+        h5.classList.add('mt-0');
+        h5.classList.add('mb-1');
+        h5.innerText = tweet.username;
+        const p = document.createElement('p');
+        p.classList.add('mt-0');
+        p.classList.add('mb-0');
+        p.classList.add('text-justify');
+        p.innerText = tweet.text;
+        const small = document.createElement('small');
+        small.classList.add('mt-0');
+        small.innerText = tweet.time;
+        div.append(h5, p, small);
+        li.append(img, div);
+        tweetList.append(li);
+    }
+}
