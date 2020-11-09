d37430f9c852e2384ce06cf2d396198fc87d518e
AmeerAdhami <aadham01@dcs.bbk.ac.uk>
2020-11-09 02:09:23 +0200

    add partials and port from env

--- app.js
+++ app.js
@@ -73,7 +73,8 @@ app.post('/analyze', async (req, res) => {
     res.send({ result: data })
 })
 
-app.listen(3000, () => {
-    console.log('listening on port 3000...');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}...`);
 })
 
